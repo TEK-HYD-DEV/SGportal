cc03d74600008f793568173c7aa20ef1886ea9a6
Balajichandu <balaji.chandu7@gmail.com>
2018-05-30 23:26:39 +0530

    first commit

---
+++ SGportal/src/test/java/com/tekzu/sgportal/SGportaloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tekzu.sgportal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SGportaloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
