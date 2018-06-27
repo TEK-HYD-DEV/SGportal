359bbf3a5b48e04532fd4f73a56f1f70cef43064
Balaji Chandu <Balaji Chandu@DESKTOP-JHKELA4>
2018-06-28 00:30:48 +0530

    Email Request

--- Result-service/src/main/java/com/tekzu/common/EmailRequest.java
+++ Result-service/src/main/java/com/tekzu/common/EmailRequest.java
@@ -1,5 +1,59 @@
 package com.tekzu.common;
 
+import java.util.Date;
+
 public class EmailRequest {
+	
+	private String emailId;
+	private String subject;
+	private String to;
+	private String cc;
+	private String body;
+	private Byte attachment;
+	private Date timeStamp;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getCc() {
+		return cc;
+	}
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public Byte getAttachment() {
+		return attachment;
+	}
+	public void setAttachment(Byte attachment) {
+		this.attachment = attachment;
+	}
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
 
 }
