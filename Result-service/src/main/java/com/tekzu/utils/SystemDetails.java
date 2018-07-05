29de42ad1bad62d25382991ba293988dd61c1773
Haritha <harithamummidivarapu94@gmail.com>
2018-07-05 21:27:59 +0530

    fetching application-properties

--- Result-service/src/main/java/com/tekzu/utils/SystemDetails.java
+++ Result-service/src/main/java/com/tekzu/utils/SystemDetails.java
@@ -1,14 +1,24 @@
 package com.tekzu.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SystemDetails {
 	
-	public static String os;
+	@Autowired
+	private Environment env;
 	
 	public void getSystemOS() {
-		os = System.getProperty("os.name");
-		System.out.println("OS Details : "+os);
+		String osName = System.getProperty("os.name");
+		String applicationVersion = env.getProperty("Application-version");
+		String serverName = env.getProperty("ServerName");
+		String applicationName = env.getProperty("ApplicationName");
+		String releaseDate = env.getProperty("ReleaseDate");
+		
+		System.out.println("osName : "+osName+" / applicationVersion : "+applicationVersion+" / serverName: "+serverName+
+				" / applicationName: "+applicationName+" / releaseDate : "+releaseDate);
+		
 	}
 }
